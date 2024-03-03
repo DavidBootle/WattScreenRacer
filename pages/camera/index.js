73dc83fd3365e1955a32d6d58c81b32f217f9f1d
Nathan Goller-Deitsch <nathan@quartzic.co>
2024-03-03 00:10:51 -0500

    camera is better <3

--- pages/camera/index.js
+++ pages/camera/index.js
@@ -1,15 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Webcam from 'react-webcam';
-import cv from '@techstark/opencv-js';
+import React, { useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import cv from "@techstark/opencv-js";
+import io from "socket.io-client";
+import useSocket from "../../src/useSocket";
+import { TrashIcon } from "@heroicons/react/20/solid";
+import parse from "color-parse";
+
+const socket = io();
 
 const videoConstraints = {
   width: 720,
   height: 480,
-  facingMode: 'user',
-  
+  facingMode: "user",
 };
 
-const WebcamCapture = ({ bounds, updateCenterOfMass }) => {
+const WebcamCapture = ({ jerseyCalibrations, sendJerseyPositions }) => {
+  useSocket(socket);
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const capturing = useRef(true);
@@ -27,67 +33,111 @@ const WebcamCapture = ({ bounds, updateCenterOfMass }) => {
     const input_image = cv.imread(image);
     const hsv = new cv.Mat();
     cv.cvtColor(input_image, hsv, cv.COLOR_RGB2HSV, 0);
-    
-    // print middle of image pixel value
-    const middle = new cv.Point(input_image.cols / 2, input_image.rows / 2);
-    const hsv_pixel = hsv.ucharPtr(middle.y, middle.x);
 
 
-    const frameMask = new cv.Mat();
-    let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [bounds.lowerHueBound, bounds.lowerSaturationBound, bounds.lowerValueBound, 0]);
-    let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [bounds.upperHueBound, bounds.upperSaturationBound, bounds.upperValueBound, 255]);
-    cv.inRange(hsv, low, high, frameMask);
+    // collect jersey positions
+    let jersey_positions = [];
 
-    const masked = new cv.Mat();
-    cv.bitwise_and(input_image, input_image, masked, frameMask);
+    // for each jersey calibration
+    for (const jersey of jerseyCalibrations) {
+
+      // convert jersey display color to hsv
+      const displayColor = parse(jersey.displayColor)
+      
+      const displayColorHsv = new cv.Scalar(
+        displayColor.values[0],
+        displayColor.values[1],
+        displayColor.values[2],
+        255
+      );
 
 
-    cv.morphologyEx(frameMask, frameMask, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(1,1)));
+      const frameMask = new cv.Mat();
+      let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [
+        jersey.hsvBounds.lowerHueBound,
+        jersey.hsvBounds.lowerSaturationBound,
+        jersey.hsvBounds.lowerValueBound,
+        0,
+      ]);
+      let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [
+        jersey.hsvBounds.upperHueBound,
+        jersey.hsvBounds.upperSaturationBound,
+        jersey.hsvBounds.upperValueBound,
+        255,
+      ]);
+      cv.inRange(hsv, low, high, frameMask);
 
+      const masked = new cv.Mat();
+      cv.bitwise_and(input_image, input_image, masked, frameMask);
 
-    // find contours
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    cv.findContours(frameMask, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+      cv.morphologyEx(
+        frameMask,
+        frameMask,
+        cv.MORPH_CLOSE,
+        cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(1, 1))
+      );
 
-    // find the largest contour
-    let maxArea = 0;
-    let maxAreaIndex = 0;
-    for (let i = 0; i < contours.size(); ++i) {
-      let area = cv.contourArea(contours.get(i));
-      if (area > maxArea) {
-        maxArea = area;
-        maxAreaIndex = i;
+      // find contours
+      let contours = new cv.MatVector();
+      let hierarchy = new cv.Mat();
+      cv.findContours(
+        frameMask,
+        contours,
+        hierarchy,
+        cv.RETR_CCOMP,
+        cv.CHAIN_APPROX_SIMPLE
+      );
+
+      // find the largest contour
+      let maxArea = 0;
+      let maxAreaIndex = 0;
+      for (let i = 0; i < contours.size(); ++i) {
+        let area = cv.contourArea(contours.get(i));
+        if (area > maxArea) {
+          maxArea = area;
+          maxAreaIndex = i;
+        }
       }
+
+      // if there is a contour
+      if (maxArea > 0) {
+        // draw center of mass on the largest contour
+        let M = cv.moments(contours.get(maxAreaIndex), false);
+        let cx = M.m10 / M.m00;
+        let cy = M.m01 / M.m00;
+        let center = new cv.Point(cx, cy);
+        cv.circle(input_image, center, 22, new cv.Scalar(0, 0, 0, 255), -1);
+        cv.circle(input_image, center, 20, displayColorHsv, -1);
+
+        // 
+        jersey_positions.push(
+          {
+            color: jersey.displayColor,
+            position: cx / input_image.cols
+          }
+        )
+      }
+
+      // cleanup
+      frameMask.delete();
+      low.delete();
+      high.delete();
+      masked.delete();
+      contours.delete();
+      hierarchy.delete();
     }
-
-    // if there is a contour
-    if (maxArea > 0) {
-      // draw center of mass on the largest contour
-      let M = cv.moments(contours.get(maxAreaIndex), false);
-      let cx = M.m10 / M.m00;
-      let cy = M.m01 / M.m00;
-      let center = new cv.Point(cx, cy);
-      cv.circle(masked, center, 20, new cv.Scalar(255, 0, 0, 255), -1);
-
-      // update center of mass
-      updateCenterOfMass({x: cx / input_image.cols, y: cy / input_image.rows});
-    }
-
 
     // Ensure the canvas element is ready for display
-    cv.imshow(canvasRef.current, masked);
-    
+    cv.imshow(canvasRef.current, input_image);
 
     // Cleanup to prevent memory leaks
     input_image.delete();
     hsv.delete();
-    frameMask.delete();
-    low.delete();
-    high.delete();
-    masked.delete();
 
-    //
+    // Push jersey positions
+    sendJerseyPositions(jersey_positions);
+
+    // Stop processing and allow the next capture
     processingImage.current = false;
   };
 
@@ -102,7 +152,7 @@ const WebcamCapture = ({ bounds, updateCenterOfMass }) => {
       }
     }
   };
-  
+
   useEffect(() => {
     capturing.current = true;
     capture();
@@ -110,10 +160,12 @@ const WebcamCapture = ({ bounds, updateCenterOfMass }) => {
       capturing.current = false; // Ensure capturing stops when the component unmounts
       clearTimeout(captureTimeoutRef.current); // Clear the ongoing timeout
     };
-  }, [bounds]); // Dependency array ensures capture restarts if bounds change
-  
+  }, [jerseyCalibrations]); // Dependency array ensures capture restarts if bounds change
+
   return (
     <>
+    <div className="flex flex-row">
+      <div className="flex-1">
       <Webcam
         audio={false}
         height={480}
@@ -122,63 +174,243 @@ const WebcamCapture = ({ bounds, updateCenterOfMass }) => {
         width={720}
         videoConstraints={videoConstraints}
       />
+      </div>
+      <div className="flex-1">
       <canvas ref={canvasRef} width="720" height="480"></canvas>
+
+      </div>
+    </div>
     </>
   );
 };
 
-
 const Slider = ({ label, value, maximum, onChange }) => (
-  <div>
+  <div className="flex flex-row gap-2">
     <label>{label}</label>
     <input
       type="range"
       min="0"
-        max={maximum}
+      max={maximum}
       value={value}
       onChange={(e) => onChange(parseInt(e.target.value, 10))}
     />
     <span>{value}</span>
   </div>
 );
-const CameraPage = () => {
 
-    // hsla(63, 50%, 60%, 1)
-  const [hsvBounds, setHsvBounds] = useState({
-    lowerHueBound: 0, // 20
-    upperHueBound: 180, // 40
-    lowerSaturationBound: 0, // 170
-    upperSaturationBound: 255, // 190
-    lowerValueBound: 0, // 140
-    upperValueBound: 255, // 160
-  })
+const JerseyCalibrationWidget = ({
+  jerseyData,
+  setJerseyData,
+  deleteJersey,
+}) => {
+  const hsvBounds = jerseyData.hsvBounds;
+  const setHsvBounds = (newHsvBounds) => {
+    setJerseyData({ ...jerseyData, hsvBounds: newHsvBounds });
+  };
+
+  return (
+    <div className="shadow bg-neutral-100 rounded p-4 flex flex-col gap-4 relative">
+      <button
+        className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-2"
+        onClick={deleteJersey}
+      >
+        <TrashIcon className="w-6 h-6 text-white" />
+      </button>
+      <input
+        className="w-64 rounded h-8 text-xl pl-2"
+        type="text"
+        value={jerseyData.name}
+        onChange={(e) => setJerseyData({ ...jerseyData, name: e.target.value })}
+      />
+
+      {/* color picker */}
+
+      <div className="flex flex-row items-center gap-2">
+        <label>Display Color</label>
+        <input
+          type="color"
+          className="w-12 h-12"
+          value={jerseyData.displayColor}
+          onChange={(e) =>
+            setJerseyData({ ...jerseyData, displayColor: e.target.value })
+          }
+        />
+      </div>
+
+      <div>
+        <Slider
+          label="Low Hue"
+          value={hsvBounds.lowerHueBound}
+          maximum={180}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, lowerHueBound: value })
+          }
+        />
+        <Slider
+          label="Hi Hue"
+          value={hsvBounds.upperHueBound}
+          maximum={180}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, upperHueBound: value })
+          }
+        />
+      </div>
+      <div>
+        <Slider
+          label="Low Sat"
+          value={hsvBounds.lowerSaturationBound}
+          maximum={255}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, lowerSaturationBound: value })
+          }
+        />
+        <Slider
+          label="Hi Sat"
+          value={hsvBounds.upperSaturationBound}
+          maximum={255}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, upperSaturationBound: value })
+          }
+        />
+      </div>
+      <div>
+        <Slider
+          label="Low Val"
+          value={hsvBounds.lowerValueBound}
+          maximum={255}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, lowerValueBound: value })
+          }
+        />
+        <Slider
+          label="Hi Val"
+          value={hsvBounds.upperValueBound}
+          maximum={255}
+          onChange={(value) =>
+            setHsvBounds({ ...hsvBounds, upperValueBound: value })
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+const CameraPage = () => {
+  // Jersey Defaults
+  const [jerseyCalibrations, setJerseyCalibrations] = useState([
+    {
+      name: "orange",
+      displayColor: "hsla(0, 50%, 60%, 1)",
+      hsvBounds: {
+        lowerHueBound: 0,
+        upperHueBound: 180,
+        lowerSaturationBound: 0,
+        upperSaturationBound: 255,
+        lowerValueBound: 0,
+        upperValueBound: 255,
+      },
+    },
+    {
+      name: "yellow",
+      displayColor: "hsla(240, 50%, 60%, 1)",
+      hsvBounds: {
+        lowerHueBound: 0,
+        upperHueBound: 180,
+        lowerSaturationBound: 0,
+        upperSaturationBound: 255,
+        lowerValueBound: 0,
+        upperValueBound: 255,
+      },
+    },
+  ]);
+
   const [centerOfMass, setCenterOfMass] = useState({
     x: 0,
-    y: 0
-  
-  })
+    y: 0,
+  });
+  useEffect(() => {
+    socket.emit("pos_update", [
+      {
+        color: "o",
+        position: centerOfMass.x,
+      },
+    ]);
+  }, [centerOfMass.x]);
 
+  return (
+    <>
+      <div className="p-2 m-2 shadow overflow-scroll">
+        <button
+          className="bg-violet-400 text-white rounded p-2 m-2"
+          onClick={() => {
+            setJerseyCalibrations([
+              ...jerseyCalibrations,
+              {
+                name: "new",
+                displayColor: "hsla(0, 50%, 60%, 1)",
+                hsvBounds: {
+                  lowerHueBound: 0,
+                  upperHueBound: 180,
+                  lowerSaturationBound: 0,
+                  upperSaturationBound: 255,
+                  lowerValueBound: 0,
+                  upperValueBound: 255,
+                },
+              },
+            ]);
+          }}
+        >
+          Add Jersey
+        </button>
+        <button className="bg-violet-400 text-white rounded p-2 m-2" onClick={() => {
+          // copy the jerseyCalibrations to clipboard
+          navigator.clipboard.writeText(JSON.stringify(jerseyCalibrations));
+        }}>Copy calibrations</button>
+        <button className="bg-violet-400 text-white rounded p-2 m-2" onClick={() => {
+          // paste the jerseyCalibrations from clipboard
+          navigator.clipboard.readText().then(text => {
+            setJerseyCalibrations(JSON.parse(text));
+          });
+        }}>Paste calibrations</button>
 
-  return <>
-  <Slider label="Lower Hue" value={hsvBounds.lowerHueBound} maximum={180} onChange={(value) => setHsvBounds({ ...hsvBounds, lowerHueBound: value })} />
-  <Slider label="Upper Hue" value={hsvBounds.upperHueBound} maximum={180} onChange={(value) => setHsvBounds({ ...hsvBounds, upperHueBound: value })} />
-    <Slider label="Lower Saturation" value={hsvBounds.lowerSaturationBound} maximum={255} onChange={(value) => setHsvBounds({ ...hsvBounds, lowerSaturationBound: value })} />
-    <Slider label="Upper Saturation" value={hsvBounds.upperSaturationBound} maximum={255} onChange={(value) => setHsvBounds({ ...hsvBounds, upperSaturationBound: value })} />
-    <Slider label="Lower Value" value={hsvBounds.lowerValueBound} maximum={255} onChange={(value) => setHsvBounds({ ...hsvBounds, lowerValueBound: value })} />
-    <Slider label="Upper Value" value={hsvBounds.upperValueBound} maximum={255} onChange={(value) => setHsvBounds({ ...hsvBounds, upperValueBound: value })} />
-    <WebcamCapture bounds={hsvBounds} updateCenterOfMass={
-      (center) => setCenterOfMass(center)
-    } />
-    <div>
-      <h2>Center of Mass</h2>
-      <p>X: {centerOfMass.x.toFixed(2)}</p>
-      <p>Y: {centerOfMass.y.toFixed(2)}</p>
-
-      {/* progress bar */}
-      <progress value={centerOfMass.x} max="1" className="w-full h-32 bg-gray-300 rounded-lg [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-slate-300 [&::-webkit-progress-value]:bg-violet-400 [&::-moz-progress-bar]:bg-violet-400"></progress>
-    </div>
-  </>
-}
-
+        <div className="flex flex-row gap-4 m-2">
+          {jerseyCalibrations.map((jerseyData, index) => (
+            <JerseyCalibrationWidget
+              key={index}
+              jerseyData={jerseyData}
+              setJerseyData={(newJerseyData) => {
+                setJerseyCalibrations([
+                  ...jerseyCalibrations.slice(0, index),
+                  newJerseyData,
+                  ...jerseyCalibrations.slice(index + 1),
+                ]);
+              }}
+              deleteJersey={
+                jerseyCalibrations.length > 1
+                  ? () => {
+                      setJerseyCalibrations([
+                        ...jerseyCalibrations.slice(0, index),
+                        ...jerseyCalibrations.slice(index + 1),
+                      ]);
+                    }
+                  : undefined
+              }
+            />
+          ))}
+        </div>
+      </div>
+      <WebcamCapture
+        jerseyCalibrations={jerseyCalibrations}
+        sendJerseyPositions={
+          (jersey_positions) => {
+            if(jersey_positions.length > 0){
+              socket.emit("pos_update", jersey_positions);
+            }
+          }
+        }
+      />
+    </>
+  );
+};
 
 export default CameraPage;
